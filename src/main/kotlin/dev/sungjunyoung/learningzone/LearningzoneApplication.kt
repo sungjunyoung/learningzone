d289c7b26db0df0de901e3c7c9f007ea9bd42324
Junyoung, Sung <wnsdud1861@gmail.com>
2022-10-15 20:31:10 +0900

    Initial commit

---
+++ src/main/kotlin/dev/sungjunyoung/learningzone/LearningzoneApplication.kt
@@ -0,0 +1,11 @@
+package dev.sungjunyoung.learningzone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningzoneApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningzoneApplication>(*args)
+}
---
+++ src/test/kotlin/dev/sungjunyoung/learningzone/LearningzoneApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.sungjunyoung.learningzone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningzoneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
